ad769ab1276e0cf8f57164357e006b225a256567
Andrii Viatyk <aviatyk@onecallcm.com>
2021-02-04 21:28:14 +0200

    Updates

---
+++ precache-manifest.170ef8e80b68b165e1ecaeac5950f699.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7cacef01e2118ccc3b4ea91521fc23",
+    "url": "./index.html"
+  },
+  {
+    "revision": "060c8c05cdd613f55b6e",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "d5e0ad8043828cc0cf39",
+    "url": "./static/js/2.28b49797.chunk.js"
+  },
+  {
+    "revision": "5bf4bc014a2abc341164f81096c622cd",
+    "url": "./static/js/2.28b49797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "060c8c05cdd613f55b6e",
+    "url": "./static/js/main.a7626a2f.chunk.js"
+  },
+  {
+    "revision": "d4a4e1ebd6b215ac0347",
+    "url": "./static/js/runtime-main.8c028954.js"
+  }
+]);
