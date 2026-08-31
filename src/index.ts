3f69bc1f5f6cb2010b9cd87490dae7410d648074
Andriy Viyatyk <viyatyk@gmail.com>
2026-08-31 19:25:19 +0300

    2.6.1

--- src/index.ts
+++ src/index.ts
@@ -185,4 +185,4 @@ export * from "./render/types";
 export * from "./types";
 
 /** Bumped on release; `AVGrid.version` and the docs both quote it. */
-export const version = "2.6.0";
+export const version = "2.6.1";
